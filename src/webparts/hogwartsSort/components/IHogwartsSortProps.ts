2ec23fd072d9cff6ee24c92f468e34975eb52e58
HarryNhu <harry.nhu@vmodev.com>
2023-02-24 00:12:21 +0700

    update logic to random house

--- src/webparts/hogwartsSort/components/IHogwartsSortProps.ts
+++ src/webparts/hogwartsSort/components/IHogwartsSortProps.ts
@@ -1,29 +1,22 @@
 import { WebPartContext } from "@microsoft/sp-webpart-base";
 export interface IHogwartsSortProps {
-    description: string;
-    userDisplayName: string;
-    email: string;
-    context: WebPartContext;
+  description: string;
+  userDisplayName: string;
+  email: string;
+  context: WebPartContext;
 }
 
 export interface IHogwartsHouse {
-    Id: number;
-    Key: string;
-    HouseNameData: string;
+  Id: number;
+  Key: string;
+  HouseNameData: string;
 }
 
-export const HOGWARTSHOUSELIST: IHogwartsHouse[] = [
-    { Id: 1, Key: "Gryffindor", HouseNameData: "Gryffindor" },
-    { Id: 2, Key: "Slytherin", HouseNameData: "Slytherin" },
-    { Id: 3, Key: "Ravenclaw", HouseNameData: "Ravenclaw" },
-    { Id: 4, Key: "Hufflepuff", HouseNameData: "Hufflepuff" },
-];
-
 export interface IStudentHogwartsHouse {
-    Id: number;
-    Title: string;
-    FullName: string;
-    Email: string;
-    HousesName: string;
-    TimeSort: Date;
+  Id: number;
+  Title: string;
+  FullName: string;
+  Email: string;
+  HousesName: string;
+  TimeSort: Date;
 }
