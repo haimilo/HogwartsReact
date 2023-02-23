b551c5539c4d328bf95e85ac0b8c3bfa590cc997
Harry Nhu <harry.nhu@vmodev.com>
2023-02-23 15:08:52 +0700

    config pnp/sp package and all api to receive data list

--- src/webparts/hogwartsSort/components/IHogwartsSortProps.ts
+++ src/webparts/hogwartsSort/components/IHogwartsSortProps.ts
@@ -1,7 +1,29 @@
+import { WebPartContext } from "@microsoft/sp-webpart-base";
 export interface IHogwartsSortProps {
-  description: string;
-  isDarkTheme: boolean;
-  environmentMessage: string;
-  hasTeamsContext: boolean;
-  userDisplayName: string;
+    description: string;
+    userDisplayName: string;
+    email: string;
+    context: WebPartContext;
+}
+
+export interface IHogwartsHouse {
+    Id: number;
+    Key: string;
+    HouseName: string;
+}
+
+export const HOGWARTSHOUSELIST: IHogwartsHouse[] = [
+    { Id: 1, Key: "Gryffindor", HouseName: "Gryffindor" },
+    { Id: 2, Key: "Slytherin", HouseName: "Slytherin" },
+    { Id: 3, Key: "Ravenclaw", HouseName: "Ravenclaw" },
+    { Id: 4, Key: "Hufflepuff", HouseName: "Hufflepuff" },
+];
+
+export interface IStudentHogwartsHouse {
+    Id: number;
+    Title: string;
+    FullName: string;
+    Email: string;
+    House: string;
+    TimeSort: Date;
 }
